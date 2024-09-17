96eca2f2ff4f7011119dbe891b160b1d44cddefc
Iago Rech Tramontin <147006617+I4g0m1t0@users.noreply.github.com>
2024-09-17 20:00:27 -0300

    Função de contagem adicionada

---
+++ Petshop_relacional/Categoria.java
@@ -0,0 +1,13 @@
+public class Categoria {
+    Integer IdCategoria;
+    String Descricao;
+
+    public Categoria(
+        Integer Idcategoria,
+        String Descricao
+    ){
+        this.IdCategoria = Idcategoria;
+        this.Descricao = Descricao;
+    }
+    
+}
---
+++ Petshop_relacional/Tutor.java
@@ -0,0 +1,18 @@
+public class Tutor {
+    Integer IdTutor;
+    String Nome;
+    String Telefone;
+    String Email;
+
+    public Tutor (
+        Integer IdTutor,
+        String Nome,
+        String Telefone,
+        String Email
+    ) {
+        this.IdTutor = IdTutor;
+        this.Nome = Nome;
+        this.Telefone = Telefone;
+        this.Email = Email;
+    }
+}
