a8f530e7c4bacfd3358851dd3c5f17861f34c0f9
Iago <iagorechtramontin@gmail.com>
2024-11-19 20:53:11 -0300

    Classes criadas da atividade final

---
+++ Atividade_final/Organizador.java
@@ -0,0 +1,32 @@
+public class Organizador extends Pessoa {
+    private String email;
+    private Notifica notificacao;
+
+    public Organizador(int id, String nome, String email, Notifica notificacao) {
+        super(id, nome); // Inicializa os atributos herdados de Pessoa
+        this.email = email;
+        this.notificacao = notificacao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Notifica getNotificacao() {
+        return notificacao;
+    }
+
+    public void setNotificacao(Notifica notificacao) {
+        this.notificacao = notificacao;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Email: " + this.email + 
+               ", Notificação: " + (notificacao != null ? notificacao.toString() : "Nenhuma");
+    }
+}
